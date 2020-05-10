b7e0e48f99ed47798684a6192d5c554f03f5ddd8
5377 <pradipamate07@gmail.com>
2020-05-10 15:14:28 +0530

    Updates

---
+++ precache-manifest.3e6425f2547f624175e7e808a001a370.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "132076303bd7c840e350f5a3062695b3",
+    "url": "/requefort%20/index.html"
+  },
+  {
+    "revision": "d9bbbd5b528176b2c84d",
+    "url": "/requefort%20/static/css/2.c3594674.chunk.css"
+  },
+  {
+    "revision": "f0da671fde14dbd1660d",
+    "url": "/requefort%20/static/css/main.409cb6ae.chunk.css"
+  },
+  {
+    "revision": "d9bbbd5b528176b2c84d",
+    "url": "/requefort%20/static/js/2.b702a4f0.chunk.js"
+  },
+  {
+    "revision": "413b595604c5d3d1d7baf18f18130db2",
+    "url": "/requefort%20/static/js/2.b702a4f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0da671fde14dbd1660d",
+    "url": "/requefort%20/static/js/main.55f16e9c.chunk.js"
+  },
+  {
+    "revision": "4495eadecb062f239c48",
+    "url": "/requefort%20/static/js/runtime-main.4f0299b1.js"
+  },
+  {
+    "revision": "0460861cba654aaea247fd38cb45570c",
+    "url": "/requefort%20/static/media/1.0460861c.jpg"
+  },
+  {
+    "revision": "87eb89de591e80f5dd19076721085c1c",
+    "url": "/requefort%20/static/media/bg-sidebar.87eb89de.jpg"
+  },
+  {
+    "revision": "a9783a3ae0887e7e6d6e3d3a8b508ac6",
+    "url": "/requefort%20/static/media/bg_video1.a9783a3a.jpg"
+  },
+  {
+    "revision": "911c395ab1ea6804f74ab94463915174",
+    "url": "/requefort%20/static/media/bg_video2.911c395a.jpg"
+  },
+  {
+    "revision": "347c8325491685931da849dcf8dd6bd3",
+    "url": "/requefort%20/static/media/brand_story.347c8325.png"
+  },
+  {
+    "revision": "a8c874b93b3d848f39a71260c57e3863",
+    "url": "/requefort%20/static/media/closedhand.a8c874b9.123"
+  },
+  {
+    "revision": "f15ed41cf3e7474cd5ad7937dcbb350b",
+    "url": "/requefort%20/static/media/customer-2.f15ed41c.png"
+  },
+  {
+    "revision": "e55f4f95ef2a1e0f770d4f173a6ab6d9",
+    "url": "/requefort%20/static/media/customer-3.e55f4f95.png"
+  },
+  {
+    "revision": "13f5ce90b7b2fae2f1537ea48fb0d0e1",
+    "url": "/requefort%20/static/media/financeicons2dd4.13f5ce90.ttf"
+  },
+  {
+    "revision": "22fe73bd7bc61cb0bb7cd1a4f7657275",
+    "url": "/requefort%20/static/media/financeicons2dd4.22fe73bd.eot"
+  },
+  {
+    "revision": "23bb5f88edf552ac03608ca9d191ed64",
+    "url": "/requefort%20/static/media/financeicons2dd4.23bb5f88.svg"
+  },
+  {
+    "revision": "e90ebaa8f1345fccbecebf225c4591a3",
+    "url": "/requefort%20/static/media/financeicons2dd4.e90ebaa8.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/requefort%20/static/media/fontawesome-webfont3e6e.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/requefort%20/static/media/fontawesome-webfont3e6e.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/requefort%20/static/media/fontawesome-webfont3e6e.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/requefort%20/static/media/fontawesome-webfont3e6e.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/requefort%20/static/media/fontawesome-webfont3e6e.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/requefort%20/static/media/fontawesome-webfontd41d.674f50d2.eot"
+  },
+  {
+    "revision": "9013e3d52e2a1cb490e058ceaffe5b56",
+    "url": "/requefort%20/static/media/founder.9013e3d5.png"
+  },
+  {
+    "revision": "3bee48d0dbbe16449357b9110860057e",
+    "url": "/requefort%20/static/media/hero4.3bee48d0.jpg"
+  },
+  {
+    "revision": "aa6bcb05fcc6999a806ba171c121a3a4",
+    "url": "/requefort%20/static/media/icomoon6e98.aa6bcb05.woff"
+  },
+  {
+    "revision": "cdeab243871786ddb93b6aeb0102c022",
+    "url": "/requefort%20/static/media/icomoon6e98.cdeab243.ttf"
+  },
+  {
+    "revision": "fc78cd40aaa615b181db09e20815f179",
+    "url": "/requefort%20/static/media/icomoon6e98.fc78cd40.eot"
+  },
+  {
+    "revision": "cea65422eb1b18f0d3ee16c24472d1f1",
+    "url": "/requefort%20/static/media/img-single2.cea65422.png"
+  },
+  {
+    "revision": "194d0aa783527bc71ae7253fe97cf9cf",
+    "url": "/requefort%20/static/media/logo-1.194d0aa7.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/requefort%20/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0393d7e9508df4e1d8de91a104a5aaa1",
+    "url": "/requefort%20/static/media/member-1-370x359.0393d7e9.jpg"
+  },
+  {
+    "revision": "707e3994379379aa75c11bf768c3ad71",
+    "url": "/requefort%20/static/media/member-2-370x359.707e3994.jpg"
+  },
+  {
+    "revision": "93a808f6861d139c94c7ce7a728b04ed",
+    "url": "/requefort%20/static/media/member-3-370x359.93a808f6.jpg"
+  },
+  {
+    "revision": "da0124b680ec5743e1f018219b2264d1",
+    "url": "/requefort%20/static/media/news1.da0124b6.jpg"
+  },
+  {
+    "revision": "b06c243f534d9c5461d16528156cd5a8",
+    "url": "/requefort%20/static/media/openhand.b06c243f.123"
+  },
+  {
+    "revision": "255b5af1b0c9e359ebccd551df1b13c5",
+    "url": "/requefort%20/static/media/pattern.255b5af1.png"
+  },
+  {
+    "revision": "11109cd84a11c5033ad760067a4bafad",
+    "url": "/requefort%20/static/media/post-4.11109cd8.jpg"
+  },
+  {
+    "revision": "f76ef4094c8d7bf11b16febf414d4946",
+    "url": "/requefort%20/static/media/post-5.f76ef409.jpg"
+  },
+  {
+    "revision": "415364b5af4932fab57cc08a467c91f1",
+    "url": "/requefort%20/static/media/post-6.415364b5.jpg"
+  },
+  {
+    "revision": "4bd6f17fedefdf76756efabf30f9cf2e",
+    "url": "/requefort%20/static/media/post-7.4bd6f17f.jpg"
+  },
+  {
+    "revision": "4dd7d98b7753544ebd5b16a6878967c3",
+    "url": "/requefort%20/static/media/post-8.4dd7d98b.jpg"
+  },
+  {
+    "revision": "fcf99160382ed53f29dcdc337ad2721e",
+    "url": "/requefort%20/static/media/post-9.fcf99160.jpg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/requefort%20/static/media/revicons90c690c6.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/requefort%20/static/media/revicons90c690c6.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/requefort%20/static/media/revicons90c690c6.2feb69cc.eot"
+  },
+  {
+    "revision": "5ad4f0d6e99dd65aba77cbef6e17c25c",
+    "url": "/requefort%20/static/media/revicons90c690c6.5ad4f0d6.svg"
+  },
+  {
+    "revision": "0838d2031fc6eaf0be9efe903cfd1bcd",
+    "url": "/requefort%20/static/media/themesflaticons5306.0838d203.ttf"
+  },
+  {
+    "revision": "2e99f64557a26e90444c292e1e98bbae",
+    "url": "/requefort%20/static/media/themesflaticons5306.2e99f645.woff"
+  },
+  {
+    "revision": "9469330fcbc01d1aad5d60642f4ef7d1",
+    "url": "/requefort%20/static/media/themesflaticons5306.9469330f.eot"
+  },
+  {
+    "revision": "aa11d1ab5108aacc7710782c9b91a93e",
+    "url": "/requefort%20/static/media/themesflaticons5306.aa11d1ab.svg"
+  },
+  {
+    "revision": "35acf6f3390fb88c7d37d15d5b1f31e3",
+    "url": "/requefort%20/static/media/video.35acf6f3.ogv"
+  },
+  {
+    "revision": "6ea460c0795ba0ff82b12eb9b81fc498",
+    "url": "/requefort%20/static/media/video.6ea460c0.mp4"
+  },
+  {
+    "revision": "9a4b0622c18f880affbdb1b756c900c9",
+    "url": "/requefort%20/static/media/video.9a4b0622.webm"
+  }
+]);
